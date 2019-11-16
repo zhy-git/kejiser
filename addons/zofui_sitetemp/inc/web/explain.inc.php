389e20d7889ed5e0a0ad89c05d270d628b691772
zhy <420998627@qq.com>
2019-11-16 12:54:14 +0800

    万能门店

--- addons/zofui_sitetemp/inc/web/explain.inc.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	global $_W,$_GPC;
-	$_W['set'] = $this->module['config'];
-	
-	
-	include $this->template('web/explain');
--- addons/zofui_sitetemp/plugin/web/appoint/inc/app.inc.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	global $_W,$_GPC;
-
-	
-
-
-	include $this->pTemplate('app');
