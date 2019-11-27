b81896534dac9cb0567319391d5e51af2acf1de1
zhy <420998627@qq.com>
2019-11-27 19:49:02 +0800

    1

--- data/config.php
+++ data/config.php
@@ -4,10 +4,10 @@ defined('IN_IA') or exit('Access Denied');
 $config = array();
 
 $config['db']['master']['host'] = 'localhost';
-$config['db']['master']['username'] = 'root';
-$config['db']['master']['password'] = '123456';
+$config['db']['master']['username'] = 'jtr168';
+$config['db']['master']['password'] = 'jtr168';
 $config['db']['master']['port'] = '3306';
-$config['db']['master']['database'] = 'jtr168_kejiser';
+$config['db']['master']['database'] = 'jtr168';
 $config['db']['master']['charset'] = 'utf8';
 $config['db']['master']['pconnect'] = 0;
 $config['db']['master']['tablepre'] = 'ims_';
