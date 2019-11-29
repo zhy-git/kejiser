95351462b0de6ca10c6c3fe9fd40080804a9db41
zhy <420998627@qq.com>
2019-11-29 21:10:06 +0800

    用户注册。

--- addons/zofui_sitetemp/inc/wxapp/1/reguser.inc.php
+++ addons/zofui_sitetemp/inc/wxapp/1/reguser.inc.php
@@ -2,20 +2,26 @@
 	defined('IN_IA') or exit('Access Denied');
 	global $_W,$_GPC;
 
- //    $content = htmlspecialchars_decode( $_GPC['addressdate'] );
-	// $data = json_decode( $content,true );
-    $user= [
-         'opendid' => $_W['i'],
-         'nickname' => $_GPC['nickname'],
-         'headimgurl' => $_GPC['headimgurl'],
-         'createtime' => time(),
-    ];
-    $result = pdo_inset('zofui_sitetemp_reguser',$user);
-    if ($result) {
-    	$this->result(0, '服务器注册用户成功',$result);
+    $hasuser = pdl_get('zofui_sitetemp_reguser',array('openid' => $_W['openid'],'uniacid' => $_W['uniacid'] ));
+    if ($hasuser) {
+    	$this->result(0, '服务器获取用户成功',$hasuser);
     }else{
-    	$this->result(0, '服务器注册用户失败',$result);
+       $user = [
+         'openid'  => $_W['openid'],
+         'uniacid' => $_W['uniacid'],
+         'nickname' => $_GPC['nickName'],
+         'headimgurl' => $_GPC['avatarUrl'],
+         'createtime' => time(),
+        ];
+	    $result = pdo_insert('zofui_sitetemp_reguser', $user);
+	    if ($result) {
+	    	$this->result(0, '服务器注册用户成功',$result);
+	    }else{
+	    	$this->result(0, '服务器注册用户失败',$result);
+	    }
     }
+
+    
 
 
 
