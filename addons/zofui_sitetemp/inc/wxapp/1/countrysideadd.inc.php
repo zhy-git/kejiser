1759fcd61d536db1a94cb31870baae68d881a39d
zhy <420998627@qq.com>
2019-11-30 16:44:17 +0800

    下乡服务文件上传

--- addons/zofui_sitetemp/inc/wxapp/1/countrysideadd.inc.php
+++ addons/zofui_sitetemp/inc/wxapp/1/countrysideadd.inc.php
@@ -4,6 +4,7 @@
 
     load()->func('file');
     $saveimgurl = file_upload($_FILES['imgfile'], 'image', '');
-	$this->result(0, '处理完图片了',array('img' => $saveimgurl));
+    $newimgurl = $_W['attachurl'].$saveimgurl['path'];
+	$this->result(0, '处理完图片了',array('img' => $newimgurl));
 
 
