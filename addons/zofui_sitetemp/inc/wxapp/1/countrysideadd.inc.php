8a6ad877fa2bd4a70e169ae72cec627040db8dca
zhy <420998627@qq.com>
2019-11-30 17:59:00 +0800

    添加服务

--- addons/zofui_sitetemp/inc/wxapp/1/countrysideadd.inc.php
+++ addons/zofui_sitetemp/inc/wxapp/1/countrysideadd.inc.php
@@ -5,13 +5,26 @@
 
     if ($_GPC['op'] == 'add') {
     	//添加
-    	$serdate = [
-            'name' => $_GPC['name'],
-            'seraddress' => $_GPC['seraddress'],
-            'img' => $_GPC['img'],
-            'content' => $_GPC['content'],
-    	];
-    	$this->result(0, '我来添加服务咯',$serdate);
+    	//判断时间 是否已经签到了
+		$getaddress = Util::getNew('zofui_sitetemp_address',array('openid'=>$_W['openid'],'uniacid'=>$_W['uniacid']));
+		$getdate = date( "Y-m-d",$getaddress['createtime']); 
+		if(date("Y-m-d",time()) == $getdate) {
+			//已签到
+			$serdate = [
+	            'name' => $_GPC['name'],
+	            'seraddress' => $_GPC['seraddress'],
+	            'img' => $_GPC['img'],
+	            'content' => $_GPC['content'],
+	            'createtime' => time(),
+	            'aid' => $getaddress['id'],
+	    	];
+	    	$this->result(0, '我来添加服务咯',$serdate);
+			
+		}elseif (empty($getaddress) || date("Y-m-d",time()) > $getdate) {
+			//还没签到
+		    $this->result(0, '您还没签到，请先签到。'); 
+		}
+    	
     	
     }else{
         //图片上传
