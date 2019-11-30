8c761f1b912e410f5c11e9a3cd220b41c35319db
zhy <420998627@qq.com>
2019-11-30 17:44:52 +0800

    添加服务

--- addons/zofui_sitetemp/inc/wxapp/1/countrysideadd.inc.php
+++ addons/zofui_sitetemp/inc/wxapp/1/countrysideadd.inc.php
@@ -1,10 +1,19 @@
 <?php
 	defined('IN_IA') or exit('Access Denied');
 	global $_W,$_GPC;
+    
 
-    load()->func('file');
-    $saveimgurl = file_upload($_FILES['imgfile'], 'image', '');
-    $newimgurl = $_W['attachurl'].$saveimgurl['path'];
-	$this->result(0, '处理完图片了',array('img' => $newimgurl));
+    if ($_GPC['op'] == 'add') {
+    	//添加
+    	$this->result(0, '我来添加服务咯',$_GPC['op']);
+    	
+    }else{
+        //图片上传
+	    load()->func('file');
+	    $saveimgurl = file_upload($_FILES['imgfile'], 'image', '');
+	    $newimgurl = $_W['attachurl'].$saveimgurl['path'];
+		$this->result(0, '处理完图片了',array('img' => $newimgurl));
+   }
+
 
 
