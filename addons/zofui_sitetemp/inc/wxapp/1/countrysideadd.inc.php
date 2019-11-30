d3ba1f6b6f6ce47d44b330c152d0dce45fd7f6da
zhy <420998627@qq.com>
2019-11-30 21:37:12 +0800

    添加服务

--- addons/zofui_sitetemp/inc/wxapp/1/countrysideadd.inc.php
+++ addons/zofui_sitetemp/inc/wxapp/1/countrysideadd.inc.php
@@ -37,10 +37,7 @@
         //图片上传
 	    load()->func('file');
 	    $saveimgurl = file_upload($_FILES['imgfile'], 'image', '');
-	    $newimgurl = array();
 	    $newimgurl = $_W['attachurl'].$saveimgurl['path'];
-	
-
 		$this->result(0, '处理完图片了',array('img' => $newimgurl));
    }
 
