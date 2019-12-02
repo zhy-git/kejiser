0d9c9a7b567449dbe3b6c4d24b2d33f31cb725ad
zhy <420998627@qq.com>
2019-12-02 15:46:37 +0800

    添加特派员。

---
+++ addons/zofui_sitetemp/inc/wxapp/1/userinfo.inc.php
@@ -0,0 +1,33 @@
+<?php
+	defined('IN_IA') or exit('Access Denied');
+	global $_W,$_GPC;
+    
+
+    if ($_GPC['op'] == 'add') {
+    	//添加特派员个人信息的
+		$info = pdo_get('zofui_sitetemp_product', array('openid'=>$_W['openid'],'uniacid'=>$_W['uniacid']));
+		if ($info) {
+			$this->result(1, '你已添加过了。');
+		}else{
+			
+			$userinfo= [
+				'uniacid' => $_W['uniacid'],
+	            'openid' => $_W['openid'],
+	            'title' => $_GPC['name'],
+	            'phone' => $_GPC['phone'],
+	            'img' => $_GPC['img'],
+	            'content' => $_GPC['content'],
+	            'createtime' => time(),
+	    	];
+	    	$result = pdo_insert('zofui_sitetemp_product',$userinfo);
+	    	if ($result) {
+	    		$this->result(0, '操作成功',$result);
+	    	}else{
+	    		$this->result(1, '操作失败');
+	    	}
+		}
+       
+   }
+
+
+
