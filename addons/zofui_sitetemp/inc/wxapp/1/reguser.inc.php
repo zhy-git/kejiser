d40c23d78573688dbfc9e551db2e0047bcdce5df
zhy <420998627@qq.com>
2019-12-12 21:36:11 +0800

    openid为空不可注册。

--- addons/zofui_sitetemp/inc/wxapp/1/reguser.inc.php
+++ addons/zofui_sitetemp/inc/wxapp/1/reguser.inc.php
@@ -8,7 +8,7 @@
          if ($user) {
              $updateinfo = pdo_update('zofui_sitetemp_reguser',array('istrue'=>3),array('id'=>$user['id']));
                  if ($updateinfo) {
-                     $this->result(0, '申请已发送，等待管理员审核。');
+                     $this->result(0, '申请已发送，等待管理员审核。',$updateinfo);
                  }else{
                     $this->result(1, '申请失败。');
                  }
