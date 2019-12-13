bd8d366fd66c60651c565b12dba0a40c342d073b
zhy <420998627@qq.com>
2019-12-13 23:02:27 +0800

    收藏

--- addons/zofui_sitetemp/inc/wxapp/1/countrysideadd.inc.php
+++ addons/zofui_sitetemp/inc/wxapp/1/countrysideadd.inc.php
@@ -125,10 +125,10 @@
         }
 
     }elseif($_GPC['op'] == 'shoucanglist'){
-    	//获取收藏列表
-    	$info = pdo_getall('zofui_sitetemp_collection', array('openid'=>$_W['openid'],'uniacid'=>$_W['uniacid']));
+    	//获取收藏列表的uid数组
+    	$info = pdo_getall('zofui_sitetemp_collection', array('openid'=>$_W['openid'],'uniacid'=>$_W['uniacid']),array('uid'));
     	if ($info) {
-    		$this->result(0, '操作成功',$info['id']);
+    		$this->result(0, '操作成功',$info);
     	}else{
     		$this->result(1, '操作失败');
     	}
