06d2d5e670b3301def945a91c011c29152e6c5ac
zhy <420998627@qq.com>
2019-12-19 14:45:24 +0800

    房产源码上传

--- addons/wdl_weihouse/inc/web/diy.inc.php
+++
@@ -1,2 +0,0 @@
-<?php
- goto JNPmF; meRsP: $selects = $this->_forms; goto JIOJx; GVCUK: $uniacid = $_W["\165\x6e\151\x61\x63\151\144"]; goto tEArN; CB6tS: global $_GPC, $_W; goto gZpas; jPamZ: $operation = !empty($_GPC["\x6f\160"]) ? $_GPC["\x6f\x70"] : "\144\151\163\160\x6c\x61\x79"; goto meRsP; JIOJx: $sec = $this->module_setting(); goto GVCUK; JNPmF: defined("\x49\x4e\x5f\111\101") or exit("\101\143\x63\x65\163\163\40\104\145\x6e\x69\145\144"); goto CB6tS; gZpas: load()->func("\x74\x70\x6c"); goto jPamZ; tEArN: $openid = $_W["\157\160\145\x6e\x69\144"];
