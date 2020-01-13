2307e30749e18564fcd163c2319af7ce9e368a29
zhy <420998627@qq.com>
2020-01-13 17:14:11 +0800

    bug

--- addons/zofui_sitetemp/inc/wxapp/1/userinfo.inc.php
+++ addons/zofui_sitetemp/inc/wxapp/1/userinfo.inc.php
@@ -58,13 +58,7 @@
    	    $info = Util::getAllDataInSingleTable('zofui_sitetemp_userinfo',$where,1,999,' `id` ASC ',true,false);
    	    $list = $info[0];
    	    foreach ($list as $key => $value) {
-    		// $list[$key]['img'] = strtok($value['img'],',');
-    		$list[$key]['img'] = explode(',', tomedia( $info['img'] )); 
-    		if (!CheckUrl($list[$key]['img'][0])) {
-				$info['img'] = 'https://www.jtr168.cn/attachment/'.$list[$key]['img'][0];  //没有域名存在
-			}else{
-				$info['img'] = $list[$key]['img'][0];  //域名存在
-			}
+    		$list[$key]['img'] = tomedia(strtok($value['img'],','));
     		$list[$key]['createtime'] = date('Y-m-d',$value['createtime']);
     	}
 
